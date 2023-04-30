561fa60ea6694845e02571947c1a2999bc398482
LuCieBeGie <105434870+LuCieBeGie@users.noreply.github.com>
2023-04-30 14:07:32 +0200

    Update script.js

--- script/script.js
+++ script/script.js
@@ -6,10 +6,21 @@ function computerPlay() {
     return computerChoice[(Math.floor(Math.random() * computerChoice.length))]
 }
 
+function playerPlay() {
+    let round = playerScore + computerScore
+    let playerSelection
+    do {
+        let player = prompt(`Round N${round + 1} Please enter your choise: rock, paper, scissors`)
+        if (player === null) continue
+        playerSelection = player.toLowerCase().trim()
+    } while (!playerSelection === '' || null)
+    return playerSelection
+}
+
 const playRound = (playerSelection, computerSelection) => {
 
-    let player = playerSelection.toUpperCase()
-    let computer = computerSelection.toUpperCase()
+    let player
+    let computer
 
     if (playerSelection == 'rock' && computerSelection == 'scissors') {
         alert(`You Won! ${player} beats ${computer}`);
@@ -38,15 +49,14 @@ const playRound = (playerSelection, computerSelection) => {
     else if (playerSelection === computerSelection) {
         alert(`OOPS! IT'S DRAW`);
     }
-    else {
-        alert(`You misspelled! Try again`);
+    else if (playerSelection !== 'rock' || 'paper' || 'scissors') {
+        alert(`You misspelled or a value is missing! Try again`);
     }
 }
 
 function game() {
     for (let i = 0; i < 5; i++) {
-        let round = playerScore + computerScore
-        let playerSelection = window.prompt(` Round N${round + 1} Please enter your choise: rock, paper, scissors`).toLowerCase()
+        let playerSelection = playerPlay()
         let computerSelection = computerPlay()
         if (playerScore + computerScore !== 5) {
             i = playerScore + computerScore
@@ -55,6 +65,10 @@ function game() {
     }
     console.log(`You ${playerScore}`);
     console.log(`Computer ${computerScore}`);
+    scoreCheck()
+}
+
+function scoreCheck() {
     if (playerScore > computerScore) {
         alert(`Game Over!!!! Congratulation!!! YOU WON!!!!!!!!
         You ${playerScore} 
@@ -70,6 +84,7 @@ function game() {
         You ${playerScore}
         Computer ${computerScore}`);
     }
+    return playerScore, computerScore
 }
 
 game();
